1683ca23c93b01a1bd74f6d18280fa8315977b94
Krutibash Nayak <132261318+krutibashn@users.noreply.github.com>
2025-06-25 22:38:53 +0530

    Update1 AddNumbers.java

--- AddNumbers.java
+++ AddNumbers.java
@@ -5,5 +5,6 @@ public class AddNumbers {
         int b = 7;
         int sum = a + b;
         System.out.println("Sum is: " + sum);
+        System.out.println("Hello I am for testing purpose");
     }
 }
