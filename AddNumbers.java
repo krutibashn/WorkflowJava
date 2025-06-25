4c7064b9e75c6f5abcd45bc1e8cf4a0aed967716
Krutibash Nayak <132261318+krutibashn@users.noreply.github.com>
2025-06-25 22:08:01 +0530

    Create AddNumbers.java

---
+++ AddNumbers.java
@@ -0,0 +1,9 @@
+// AddNumbers.java
+public class AddNumbers {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+        System.out.println("Sum is: " + sum);
+    }
+}
